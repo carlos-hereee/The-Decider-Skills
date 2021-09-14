a9abc438e390eb3b3a1bbd5ccf523bc883372513
ch04937 <97hernandez.c@gmail.com>
2021-09-14 09:41:40 -0700

    global navigation

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
 import { StatusBar } from "expo-status-bar";
 import { HandbookState } from "./src/utils/Context.js";
+import { navigationRef } from "./src/utils/RootNavigation.js";
 import Homepage from "./src/pages/Homepage";
 import Handbook from "./src/pages/Handbook";
 import TheFizz from "./src/pages/TheFizz";
+import ClaimBadge from "./src/pages/ClaimBadge";
 import Footer from "./src/components/Footer";
 
 const Stack = createNativeStackNavigator();
@@ -22,14 +24,15 @@ export default function App() {
     return Promise.all(cacheImages);
   };
   return isReady ? (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <HandbookState>
         <Stack.Navigator
-          initialRouteName="Home"
+          initialRouteName="ClaimBadge"
           screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={Homepage} />
           <Stack.Screen name="Handbook" component={Handbook} />
           <Stack.Screen name="TheFizz" component={TheFizz} />
+          <Stack.Screen name="ClaimBadge" component={ClaimBadge} />
         </Stack.Navigator>
         <StatusBar style="auto" />
         <Footer />
