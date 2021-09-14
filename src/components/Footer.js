607bf6e834b79855239e139fcba90a737b8ce4ec
ch04937 <97hernandez.c@gmail.com>
2021-09-14 09:42:01 -0700

    footer matches app background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import { Text } from "react-native";
 
 const Footer = () => (
-  <Text style={{ textAlign: "center", marginTop: "auto" }}>
+  <Text
+    style={{
+      textAlign: "center",
+      marginTop: "auto",
+      backgroundColor: "#EFF5FA",
+    }}>
     © Copyright 2016-2020 The Decider® Limited • Guernsey Registry 64654
   </Text>
 );
